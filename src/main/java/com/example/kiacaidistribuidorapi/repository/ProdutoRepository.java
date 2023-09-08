6e8acf0c2a2136332785858ee3f179810a1143df
jefflenno1 <jeffersonlennon1@outlook.com>
2023-09-08 20:56:20 -0300

    configurando o mybatis para fucionar em harmonia com o jpa

--- src/main/java/com/example/kiacaidistribuidorapi/repository/ProdutoRepository.java
+++ src/main/java/com/example/kiacaidistribuidorapi/repository/ProdutoRepository.java
@@ -2,14 +2,12 @@ package com.example.kiacaidistribuidorapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.example.kiacaidistribuidorapi.models.Produto;
 
 import jakarta.transaction.Transactional;
 
 @Transactional
-@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	
 	@Query(value = "SELECT * FROM PRODUTOS WHERE ID = :id", nativeQuery = true)
